ecad5880e6bd60e9434d411490250646bd57a3c1
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-08 20:08:29 -0700

    Optional syntactical elements {}.

--- src/test/scala/com/equalinformation/fpps/test/scala/UnitSpec.scala
+++ src/test/scala/com/equalinformation/fpps/test/scala/UnitSpec.scala
@@ -6,7 +6,4 @@ import org.scalatest._
  * Created by bpupadhyaya on 9/8/15.
  */
 abstract class UnitSpec extends FlatSpec with Matchers with OptionValues with
-Inside with Inspectors {
-
-
-}
+Inside with Inspectors
