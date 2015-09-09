33d4f715d24022eb62f1b1e7a7854be4bad10d8d
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-08 20:19:59 -0700

    Renamed test class.

--- src/test/scala/com/equalinformation/fpps/test/scala/LogicalAndTest.scala
+++ src/test/scala/com/equalinformation/fpps/test/scala/LogicalAndTest.scala
@@ -5,7 +5,7 @@ import collection.mutable.Stack
 /**
  * Created by bpupadhyaya on 9/8/15.
  */
-class FPPSSpec extends UnitSpec {
+class LogicalAndTest extends UnitSpec {
   //TODO Custom tests here
 
 }
