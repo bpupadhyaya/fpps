f36af09d9b33702243b1ad6aad8c2d63e6ba8a9c
bpupadhyaya <bpupadhyaya@gmail.com>
2015-09-08 19:24:21 -0700

    Unit testing added.

---
+++ src/test/scala/com/equalinformation/fpps/test/scala/FPPSSpec.scala
@@ -0,0 +1,11 @@
+package com.equalinformation.fpps.test.scala
+
+import collection.mutable.Stack
+
+/**
+ * Created by bpupadhyaya on 9/8/15.
+ */
+class FPPSSpec extends UnitSpec {
+  //TODO Custom tests here
+
+}
