7128fbf96a5629795669708452e7ed093dba0160
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-28 22:45:08 -0700

    Functions as objects: functions as classes and function values as objects.

--- src/main/scala/com/equalinformation/fpps/week4/List.scala
+++ src/main/scala/com/equalinformation/fpps/week4/List.scala
@@ -21,3 +21,14 @@ class Nil[T] extends List[T] {
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 
 }
+
+object List {
+  // List(1, 2) = List.apply(1, 2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+
+  // List(1)
+  def apply[T](x: T): List[T] = new Cons(x, new Nil)
+
+  // List()
+  def apply[T]() = new Nil
+}
