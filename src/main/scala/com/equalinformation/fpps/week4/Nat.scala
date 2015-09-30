4f93e28cef9ad0b0adcf0c28a453525b5902e4bd
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-30 16:51:37 -0700

    Type.

--- src/main/scala/com/equalinformation/fpps/week4/Nat.scala
+++ src/main/scala/com/equalinformation/fpps/week4/Nat.scala
@@ -17,7 +17,7 @@ abstract class Nat {
 
 object Zero extends Nat {
   def isZero = true
-  def predecessor = throw new Error("0.predessor")
+  def predecessor = throw new Error("0.predecessor")
   def +(that: Nat) = that
   def -(that: Nat) = if (that.isZero) this else throw new Error("negative number")
 }
