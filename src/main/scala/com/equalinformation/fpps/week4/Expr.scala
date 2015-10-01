dab447107c45f249ae7e97236cfa61274c7b7ca3
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-10-01 14:21:38 -0700

    Sum added.

--- src/main/scala/com/equalinformation/fpps/week4/Expr.scala
+++ src/main/scala/com/equalinformation/fpps/week4/Expr.scala
@@ -20,3 +20,19 @@ class Number(n: Int) extends Expr {
   def leftOp: Expr = throw new Error("Number.leftOp")
   def rightOp: Expr = throw new Error("Number.rightOp")
 }
+
+class Sum(e1: Expr, e2: Expr) extends Expr {
+  def isNumber: Boolean = false
+  def isSum: Boolean = false
+  def numValue: Int = throw new Error("Sum.numValue")
+  def leftOp: Expr = e1
+  def rightOp: Expr = e2
+
+//  def eval(e: Expr): Int = {
+//    if(e.isNumber) e.numValue
+//    else if (e.isSum) eval(e.leftOp) + eval(e.rightOp)
+//    else throw new Error("Unknown expression "+e)
+//  }
+}
+
+
